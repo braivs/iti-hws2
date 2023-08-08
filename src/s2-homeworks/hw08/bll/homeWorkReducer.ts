ce17b0b5c64446d73b4ddd4bb7915ea45b7c9a48
Briws <brightwiths@gmail.com>
2023-08-08 20:23:54 +0400

    HW8 complete

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,17 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
             // sort() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            let stateCopy = [...state]
+            stateCopy.sort((a, b) => b.name > a.name ? -1 : 1)
+            return action.payload === 'up' ? stateCopy : stateCopy.reverse()
         }
         case 'check': {
             // filter() создаёт новый массив? или нужно в ручную?...
-            return state // need to fix
+            return state.filter(el => el.age >= 18)
         }
         default:
             return state
